d6096ccb6560b8d786a0cf016829f6297fbf44d6
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-15 12:14:54 +0200

    Tested type declaration by inference of a string

--- app.ts
+++ app.ts
@@ -1,3 +1,5 @@
 // string
 let myName = 'Munalula';
-///// myName = 23; will generate and error because typescript infers the type from initialized value.
+// myName = 23; will generate and error because typescript infers the type from initialized value.
+ let myNumber = 23;
+ /// same goes for myNumber = "23";
