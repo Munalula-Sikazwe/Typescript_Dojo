cd610879ec6e3f58713dbd0c19398fa0e1c57485
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-15 13:06:02 +0200

    Functions can have return types and NaN is actually a number who knew.

--- app.ts
+++ app.ts
@@ -3,3 +3,7 @@ let myName = 'Munalula';
 // myName = 23; will generate and error because typescript infers the type from initialized value.
  let myNumber = 23;
  /// same goes for myNumber = "23";
+function  multiply(firstNumber:number,secondNumber:number):number{
+ return firstNumber * secondNumber;
+}
+console.log(multiply(2,5));
